0f8919b91bd75427255b64c710b635c64730a794
pooriya <adibradp.enginear@gmail.com>
2024-12-26 13:59:18 +0330

    th e person crud urls be created

--- Ecommerce/accounts/urls.py
+++ Ecommerce/accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import create_person, raed_person, delete_person, update_person
 
 urlpatterns = [
-
+    path('create/', create_person, name='create'),
+    path('read/', raed_person, name='raed'),
+    path('delete/', delete_person, name='delete'),
+    path('update/', update_person, name='update'),
 ]
